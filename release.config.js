08ebde6bab6b9ee7adcc306f690886afe691fa1a
Matt Hayes <matt@mysterycommand.com>
2018-07-15 14:58:04 -0400

    ci(release.config.js): add git and changelog plugins to release

--- release.config.js
+++ release.config.js
@@ -1,5 +1,25 @@
 module.exports = {
-  verifyConditions: ['@semantic-release/github'],
-  prepare: [],
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
+  prepare: [
+    '@semantic-release/changelog',
+    {
+      path: '@semantic-release/git',
+      message: `[${
+        nextRelease.version
+      }](https://github.com/fartts/fartts/tree/${
+        nextRelease.version
+      }) - ${new Date().toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      })} [skip ci]\n\n${nextRelease.notes}`,
+    },
+  ],
   publish: ['@semantic-release/github'],
 };
