6e542524457f61d1362b83fceda1354c59a2b390
Matt Hayes <matt@mysterycommand.com>
2018-07-16 00:09:57 -0400

    chore(release.config.js): update release message string

--- release.config.js
+++ release.config.js
@@ -8,19 +8,8 @@ module.exports = {
     '@semantic-release/changelog',
     {
       path: '@semantic-release/git',
-      message: [
-        '[${',
-        '  nextRelease.version',
-        '}](https://github.com/fartts/fartts/tree/${',
-        '  nextRelease.version',
-        '}) - ${new Date().toLocaleDateString("en-US", {',
-        '  year: "numeric",',
-        '  month: "short",',
-        '  day: "numeric",',
-        '  hour: "numeric",',
-        '  minute: "numeric",',
-        '})} [skip ci]\n\n${nextRelease.notes}',
-      ].join('\n'),
+      message:
+        '[${nextRelease.version}](https://github.com/fartts/fartts/tree/${nextRelease.version}) - ${new Date().toLocaleDateString("en-US", {year: "numeric",month: "short",day: "numeric",hour: "numeric",minute: "numeric",})} [skip ci]\n\n${nextRelease.notes}',
     },
   ],
   publish: ['@semantic-release/github'],
