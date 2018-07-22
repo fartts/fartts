bb70e21f77b0eb3a81aa4e68c9dd90283aa02560
Matt Hayes <matt@mysterycommand.com>
2018-07-22 12:58:00 -0400

    feat(experiments/webgl): adds initial setup for working with webgl
    feeling very distracted this morning

--- src/app/experiments/webgl/main.ts
+++ src/app/experiments/webgl/main.ts
@@ -1,4 +1,31 @@
 import './main.css';
 
-import { π } from '@fartts/lib/math';
-console.log(`π: ${π}`); // tslint:disable-line no-console
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const canvasContext = canvas.getContext('webgl') as WebGLRenderingContext;
+
+const scale = 5;
+const width = canvas.clientWidth / scale;
+const height = canvas.clientHeight / scale;
+const centerX = width / 2;
+const centerY = height / 2;
+
+canvas.width = width;
+canvas.height = height;
+
+console.log(canvasContext, centerX, centerY); // tslint:disable-line no-console
+
+enum WebGLShaderType {
+  Vertex = canvasContext.VERTEX_SHADER,
+  Fragment = canvasContext.FRAGMENT_SHADER,
+}
+
+function compileShader(
+  context: WebGLRenderingContext,
+  type: WebGLShaderType,
+  source: string,
+): WebGLShader | null {
+  const shader = context.createShader(type);
+  context.shaderSource(shader, source);
+  context.compileShader(shader);
+  return shader;
+}
