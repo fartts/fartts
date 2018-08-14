366e2776ec12e43003c6034904925b2c99d698a7
Matt Hayes <matt@mysterycommand.com>
2018-08-13 23:42:08 -0400

    feat(lab): program boilerplate

--- src/app/lab/03-waveforms/main.ts
+++ src/app/lab/03-waveforms/main.ts
@@ -6,3 +6,15 @@ const gl = c.getContext('webgl', {
 }) as WebGLRenderingContext;
 
 gl.clearColor(0, 0, 0, 1);
+
+function init(): void {
+  console.log('init'); // tslint:disable-line no-console
+}
+
+function draw(t: number): void {
+  console.log('draw', t); // tslint:disable-line no-console
+  requestAnimationFrame(draw);
+}
+
+init();
+requestAnimationFrame(draw);
