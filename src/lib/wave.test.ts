6999512f1612a6beec4e3009eaca738867f7c3c4
Matt Hayes <matt@mysterycommand.com>
2018-08-13 21:39:32 -0400

    feat(lab): add a step function
    it's like an integer-only wave function

--- src/lib/wave.test.ts
+++ src/lib/wave.test.ts
@@ -1,5 +1,5 @@
 import { cos, sin } from './math';
-import { wave } from './wave';
+import { step, wave } from './wave';
 
 // just to keep the table below consice when testing defaults
 const u = undefined;
@@ -7,20 +7,35 @@ const u = undefined;
 test.each`
   fn     | p       | min   | max  | o      | t       | expected
   ${sin} | ${u}    | ${u}  | ${u} | ${u}   | ${0}    | ${0}
-  ${sin} | ${1000} | ${-1} | ${1} | ${0}   | ${0}    | ${0}
   ${sin} | ${1000} | ${-1} | ${1} | ${0}   | ${250}  | ${1}
-  ${sin} | ${1000} | ${-1} | ${1} | ${0}   | ${500}  | ${0}
   ${sin} | ${1000} | ${-1} | ${1} | ${0}   | ${750}  | ${-1}
-  ${sin} | ${1000} | ${-1} | ${1} | ${0}   | ${1000} | ${0}
   ${sin} | ${2000} | ${0}  | ${3} | ${500} | ${1000} | ${0}
   ${cos} | ${u}    | ${u}  | ${u} | ${u}   | ${0}    | ${1}
-  ${cos} | ${1000} | ${-1} | ${1} | ${0}   | ${0}    | ${1}
   ${cos} | ${1000} | ${-1} | ${1} | ${0}   | ${250}  | ${0}
-  ${cos} | ${1000} | ${-1} | ${1} | ${0}   | ${500}  | ${-1}
   ${cos} | ${1000} | ${-1} | ${1} | ${0}   | ${750}  | ${0}
-  ${cos} | ${1000} | ${-1} | ${1} | ${0}   | ${1000} | ${1}
   ${cos} | ${2000} | ${0}  | ${3} | ${500} | ${1000} | ${1.5}
-`('wave', ({ fn, p, min, max, o, t, expected }) => {
-  const waveFn = wave(fn, p, min, max, o);
-  expect(waveFn(t)).toBeCloseTo(expected);
-});
+`(
+  'wave over period: $p, between: $min & $max, with offset: $o, at time $t',
+  ({ fn, p, min, max, o, t, expected }) => {
+    const waveFn = wave(fn, p, min, max, o);
+    expect(waveFn(t)).toBeCloseTo(expected);
+  },
+);
+
+test.each`
+  fn     | p       | min   | max  | o      | t       | expected
+  ${sin} | ${u}    | ${u}  | ${u} | ${u}   | ${0}    | ${0}
+  ${sin} | ${1000} | ${-1} | ${1} | ${0}   | ${250}  | ${1}
+  ${sin} | ${1000} | ${-1} | ${1} | ${0}   | ${750}  | ${-1}
+  ${sin} | ${2000} | ${0}  | ${3} | ${500} | ${1000} | ${0}
+  ${cos} | ${u}    | ${u}  | ${u} | ${u}   | ${0}    | ${1}
+  ${cos} | ${1000} | ${-1} | ${1} | ${0}   | ${250}  | ${0}
+  ${cos} | ${1000} | ${-1} | ${1} | ${0}   | ${750}  | ${-1}
+  ${cos} | ${2000} | ${0}  | ${3} | ${500} | ${1000} | ${1}
+`(
+  'step over period: $p, between: $min & $max, with offset: $o, at time $t',
+  ({ fn, p, min, max, o, t, expected }) => {
+    const stepFn = step(fn, p, min, max, o);
+    expect(stepFn(t)).toBeCloseTo(expected);
+  },
+);
--- src/lib/wave.ts
+++ src/lib/wave.ts
@@ -1,4 +1,4 @@
-import { ππ } from './math';
+import { floor, ππ } from './math';
 
 type TrigFunction = (radians: number) => number;
 type WaveFunction = (timestamp: number) => number;
@@ -28,10 +28,49 @@ export function wave(
   max = 1,
   o = 0,
 ): WaveFunction {
+  /**
+   * peak amplitude (not peak-to-peak amplitude)
+   * @see https://en.wikipedia.org/wiki/Amplitude
+   */
   const amp = (max - min) / 2;
+
+  /**
+   * radians per period (angular frequency)
+   * @see https://en.wikipedia.org/wiki/Angular_frequency
+   */
   const rpp = ππ / p;
+
   return (t: number) => {
-    const ots = o + t;
+    const ots = o + t; // offset timestamp
     return amp * (1 + fn(ots * rpp)) + min;
   };
+}
+
+/**
+ * ## step
+ * this utility function returns a step function (like a integer-only wave
+ * function) that takes a timestamp (optionally offset by `o`) and returns an
+ * integer value between `min` and `max` for the period `p`
+ *
+ * @export
+ * @param {TrigFunction} fn the function to generate the stepped wave (takes a
+ * value in radians and returns a value between -1 and 1 at some interval)
+ * @param {number} [p=1000] the period for this stepped wave (in milliseconds)
+ * @param {*} [min=-1] the minimum value for the generated stepped wave
+ * @param {number} [max=1] the maximum value for the generated stepped wave
+ * @param {number} [o=0] an optional offset (in milliseconds) at which to start
+ * the stepped wave
+ * @returns {WaveFunction} a stepped (integer-only) wave function that takes a
+ * timestamp (optionally offset by `o`) and returns an integer value between
+ * `min` and `max` for the period `p`
+ */
+export function step(
+  fn: TrigFunction,
+  p = 1000,
+  min = -1,
+  max = 1,
+  o = 0,
+): WaveFunction {
+  const waveFn = wave(fn, p, min, max, o);
+  return (t: number) => floor(waveFn(t));
 }
