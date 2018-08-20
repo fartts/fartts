6d556a32c5226f30f3e866848945fa63048858fc
Matt Hayes <matt@mysterycommand.com>
2018-08-19 22:28:26 -0400

    feat: start a new experiment by shuffling some stuff around

--- fart.d.ts
+++ fart.d.ts
@@ -1,3 +1,4 @@
 declare module '*.frag';
+declare module '*.glsl';
 declare module '*.vert';
 declare module '*.json';
---
+++ src/app/lab/04-vectors/webgl/program.ts
@@ -0,0 +1,32 @@
+export function validate(
+  gl: WebGLRenderingContext,
+  program: WebGLProgram | null,
+): void {
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+  if (!success) {
+    throw new Error(
+      `program (${program}) failed to link:\n${gl.getProgramInfoLog(program)}`,
+    );
+
+    gl.deleteProgram(program);
+  }
+}
+
+export function link(
+  gl: WebGLRenderingContext,
+  vert: WebGLShader,
+  frag: WebGLShader,
+): WebGLProgram {
+  const program = gl.createProgram();
+
+  gl.attachShader(program, vert);
+  gl.attachShader(program, frag);
+  gl.linkProgram(program);
+
+  if (process.env.NODE_ENV === 'development') {
+    validate(gl, program);
+  }
+
+  return program as WebGLProgram;
+}
