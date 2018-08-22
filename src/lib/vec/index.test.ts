d905498e04363613fe140935b5192b65f1106912
Matt Hayes <matt@mysterycommand.com>
2018-08-22 00:48:00 -0400

    refactor(lib): what was the last thing I said? idk, it works now I think
    so tired

--- src/lib/vec/index.test.ts
+++ src/lib/vec/index.test.ts
@@ -8,13 +8,23 @@ describe('vec/index', () => {
     expect(() => vec2(1, 2, 3)).toThrow('too many arguments');
 
     expect(vec3).toBeDefined();
-
     const yz = vec3(1, 2, 3).yz;
     expect(yz[0]).toBe(2);
     expect(yz[1]).toBe(3);
     expect(yz.length).toBe(2);
 
     expect(vec4).toBeDefined();
+    const bgr = vec4(yz, 1, 0).bgr;
+    expect(bgr[0]).toBe(1);
+    expect(bgr[1]).toBe(3);
+    expect(bgr[2]).toBe(2);
+
+    const stpq = vec4(1, 2, 3, 4);
+    stpq.st = stpq.pq;
+    expect(stpq[0]).toBe(3);
+    expect(stpq[1]).toBe(4);
+    expect(stpq[2]).toBe(3);
+    expect(stpq[3]).toBe(4);
   });
 
   test('config', () => {
--- src/lib/vec/index.ts
+++ src/lib/vec/index.ts
@@ -1,5 +1,6 @@
+// const { isArray } = Array;
 const { get, set } = Reflect;
-const { concat } = [];
+const { concat, slice } = [];
 
 const baseKeys = [
   ['x', 'y', 'z', 'w'],
@@ -50,11 +51,12 @@ export const indicesByKey = new Map(
 function getSwizzled(target: IVector, prop: string): IVector {
   const factory = vecs[prop.length - 2];
   const keys = prop.split('');
+
   return factory(
     keys.map(k => {
       const i = indicesByKey.has(k) && indicesByKey.get(k);
 
-      if (i === undefined || typeof i !== 'number') {
+      if (typeof i !== 'number') {
         throw new Error();
       }
 
@@ -63,18 +65,29 @@ function getSwizzled(target: IVector, prop: string): IVector {
   );
 }
 
-function vec(size: number, args: Components[]): IVector {
-  const components = concat.apply([], args);
+function setSwizzled(target: IVector, prop: string, value: Components) {
+  const keys = prop.split('');
+  const values = concat.apply([], value);
 
-  if (components.length < size) {
+  if (keys.length < values.length) {
     throw new Error('not enough data provided for construction');
   }
 
-  if (components.length > size) {
+  if (keys.length > values.length) {
     throw new Error('too many arguments');
   }
 
-  return new Float32Array(components) as IVector;
+  keys.forEach(k => {
+    const i = indicesByKey.has(k) && indicesByKey.get(k);
+
+    if (typeof i !== 'number') {
+      throw new Error();
+    }
+
+    target[i] = values[i];
+  });
+
+  return true;
 }
 
 const handler: ProxyHandler<IVector> = {
@@ -86,10 +99,31 @@ const handler: ProxyHandler<IVector> = {
 
   set(target: IVector, prop: PropertyKey, value: Components) {
     return typeof prop === 'string' && swizzledKeys.has(prop)
-      ? false
+      ? setSwizzled(target, prop, value)
       : set(target, prop, value);
   },
 };
+
+function flatten(args: Components[]): ArrayLike<number> {
+  return concat.apply(
+    [],
+    args.map(arg => (typeof arg === 'number' ? arg : slice.call(arg))),
+  );
+}
+
+function vec(size: number, args: Components[]): IVector {
+  const components = flatten(args);
+
+  if (components.length < size) {
+    throw new Error('not enough data provided for construction');
+  }
+
+  if (components.length > size) {
+    throw new Error('too many arguments');
+  }
+
+  return new Float32Array(components) as IVector;
+}
 
 const vecs = new Array(4)
   .fill(true)
