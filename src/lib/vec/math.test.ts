5515b988d92c27934c1cdf4918b501f6669a9bb1
Matt Hayes <matt@mysterycommand.com>
2018-09-03 13:38:38 -0400

    test(lib/vec/math): update tests to use .each/table syntax, stub out other functions' tests

--- src/lib/vec/math.test.ts
+++ src/lib/vec/math.test.ts
@@ -1,38 +1,92 @@
+import Vector from '.';
 import { vec2, vec3, vec4 } from './factories';
-import { dot, ρ, θ } from './math';
+import { dot, ρ, θ, clone, add, sub, mul, div, norm, lerp } from './math';
 import { hypot, sqrt, π, toRadians } from '../math';
 
 describe('@fartts/lib/vec/math', () => {
+  const { origin, left } = Vector;
+
   const v2 = vec2(2, 2);
   const v3 = vec3(3, 3, 3);
   const v4 = vec4(4, 4, 4, 4);
 
-  test('dot', () => {
-    expect(() => dot(v2, v3)).toThrow('expected vectors of equal length');
-    expect(() => dot(v3, v4)).toThrow('expected vectors of equal length');
+  test.each`
+    a     | b     | result  | error
+    ${v2} | ${v3} | ${null} | ${'expected vectors of equal length'}
+    ${v3} | ${v4} | ${null} | ${'expected vectors of equal length'}
+    ${v2} | ${v2} | ${8}    | ${null}
+    ${v3} | ${v3} | ${27}   | ${null}
+    ${v4} | ${v4} | ${64}   | ${null}
+  `(
+    'dot($a, $b) should be $result or throw $error',
+    ({ a, b, result, error }) => {
+      if (error) {
+        expect(() => dot(a, b)).toThrow(error);
+      } else {
+        expect(dot(a, b)).toBe(result);
+      }
+    },
+  );
 
-    expect(dot(v2, v2)).toBe(8);
-    expect(dot(v3, v3)).toBe(27);
-    expect(dot(v4, v4)).toBe(64);
+  test.each`
+    v     | result
+    ${v2} | ${sqrt(8)}
+    ${v2} | ${hypot(...v2)}
+    ${v3} | ${sqrt(27)}
+    ${v3} | ${hypot(...v3)}
+    ${v4} | ${sqrt(64)}
+    ${v4} | ${hypot(...v4)}
+  `('ρ($v) should be $result', ({ v, result }) => {
+    expect(ρ(v)).toBe(result);
   });
 
-  test('ρ', () => {
-    expect(ρ(v2)).toBe(sqrt(8));
-    expect(ρ(v2)).toBe(hypot(...v2));
+  test.each`
+    a     | b            | result                           | error
+    ${v2} | ${origin[0]} | ${null}                          | ${'cannot get θ between vectors'}
+    ${v3} | ${origin[1]} | ${null}                          | ${'cannot get θ between vectors'}
+    ${v4} | ${origin[2]} | ${null}                          | ${'cannot get θ between vectors'}
+    ${v2} | ${left[0]}   | ${π / 4}                         | ${null}
+    ${v2} | ${left[0]}   | ${toRadians(45)}                 | ${null}
+    ${v3} | ${left[1]}   | ${π / 3.2885355431}              | ${null}
+    ${v3} | ${left[1]}   | ${toRadians(54.735610317245346)} | ${null}
+    ${v4} | ${left[2]}   | ${π / 3}                         | ${null}
+    ${v4} | ${left[2]}   | ${toRadians(60)}                 | ${null}
+  `(
+    'θ($a, $b) should be $result or throw $error',
+    ({ a, b, result, error }) => {
+      if (error) {
+        expect(() => θ(a, b)).toThrow(error);
+      } else {
+        expect(θ(a, b)).toBeCloseTo(result);
+      }
+    },
+  );
 
-    expect(ρ(v3)).toBe(sqrt(27));
-    expect(ρ(v3)).toBe(hypot(...v3));
-
-    expect(ρ(v4)).toBe(sqrt(64));
-    expect(ρ(v4)).toBe(hypot(...v4));
+  test('clone', () => {
+    // expect(clone()).toEqual();
   });
 
-  test('θ', () => {
-    const originV2 = vec2([0, 0]);
-    const leftV2 = vec2([1, 0]);
+  test('add', () => {
+    // expect(add()).toEqual();
+  });
 
-    expect(() => θ(v2, originV2)).toThrow('cannot get θ between vectors');
-    expect(θ(v2, leftV2)).toBeCloseTo(π / 4);
-    expect(θ(v2, leftV2)).toBeCloseTo(toRadians(45));
+  test('sub', () => {
+    // expect(sub()).toEqual();
+  });
+
+  test('mul', () => {
+    // expect(mul()).toEqual();
+  });
+
+  test('div', () => {
+    // expect(div()).toEqual();
+  });
+
+  test('norm', () => {
+    // expect(norm()).toEqual();
+  });
+
+  test('lerp', () => {
+    // expect(lerp()).toEqual();
   });
 });
