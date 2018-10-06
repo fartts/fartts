88ca64815dc25ca32a85c0329c4ee619a0be72ef
Matt Hayes <matt@mysterycommand.com>
2018-10-06 15:23:02 -0400

    fix(build): adds stuff to make parcel use Babel 7 to compile TypeScript

--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,7 @@ module.exports = {
   trailingComma: 'all',
   overrides: [
     {
-      files: 'ts*.json',
+      files: ['.babelrc', 'ts*.json'],
       options: { parser: 'json-stringify' },
     },
   ],
