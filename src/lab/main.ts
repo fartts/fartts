99bb6d87920ad5a47a5e9e61d603416b2f093b1d
Matt Hayes <matt@mysterycommand.com>
2018-11-27 16:57:10 -0500

    refactor(spike): remove the spike stuff entirely

--- src/lab/main.ts
+++ src/lab/main.ts
@@ -1,7 +1,1 @@
 import './main.css';
-import { vec2, vec3, vec4 } from '../lib/vec';
-
-console.log(vec2(0)); // tslint:disable-line no-console
-console.log(vec3(11, 1, 1).slice()); // tslint:disable-line no-console
-console.log(vec4(2, 2, vec2(2, 2)).map(val => val * 2)); // tslint:disable-line no-console
-console.log(vec3().xy); // tslint:disable-line no-console
