a01c021521137288368ff289da8f774fa819963c
Matt Hayes <matt@mysterycommand.com>
2018-11-28 00:11:36 -0500

    refactor(lib/vec/props): rewrite createProps to be 'flatter' and pass state around rather than rely
    idk, feels better I guess

--- src/lib/vec/props.ts
+++ src/lib/vec/props.ts
@@ -5,90 +5,101 @@ import Vec4 from './4';
 import { isFinite } from '../math';
 import { toSwizzledSet, toKeyIndexMap } from './keys';
 
-export function createProps<I extends Float32Array>(baseKeys: string[][]) {
+const Vec = [Vec2, Vec3, Vec4];
+
+function getByKey<V extends Float32Array>(
+  target: V,
+  key: string,
+  map: Map<string, number>,
+): number {
+  const i = map.get(key) as number;
+  return target[i];
+}
+
+function getterFor<V extends Float32Array>(
+  key: string,
+  map: Map<string, number>,
+):
+  | ((this: V) => number)
+  | ((this: V) => Vec2)
+  | ((this: V) => Vec3)
+  | ((this: V) => Vec4) {
+  const { length } = key;
+
+  return length === 1
+    ? function get(this: V) {
+        return getByKey<V>(this, key, map);
+      }
+    : function get(this: V) {
+        const args = key.split('').map(k => getByKey<V>(this, k, map));
+        return new Vec[length - 2](args);
+      };
+}
+
+function setByKey<V extends Float32Array>(
+  target: V,
+  key: string,
+  map: Map<string, number>,
+  value: number,
+): void {
+  if (!isFinite(value)) {
+    throw new Error(
+      `Cannot assign ${typeof value} value "${value}" to a component of ${
+        target.constructor.name
+      }`,
+    );
+  }
+
+  const i = map.get(key) as number;
+  target[i] = value;
+}
+
+function setterFor<V extends Float32Array>(
+  key: string,
+  map: Map<string, number>,
+):
+  | ((this: V, value: number) => void)
+  | ((this: V, value: ArrayLike<number>) => void) {
+  const { length } = key;
+
+  return length === 1
+    ? function set(this: V, value: number) {
+        setByKey<V>(this, key, map, value);
+      }
+    : function set(this: V, value: ArrayLike<number>) {
+        if (length !== value.length) {
+          throw new Error(
+            [
+              'Dimension mismatch',
+              `Cannot assign ${
+                value.length
+              }-component value: ${value} to ${length}-component key ${key}`,
+            ].join('\n'),
+          );
+        }
+
+        key.split('').forEach((k, i) => setByKey<V>(this, k, map, value[i]));
+      };
+}
+
+export function createProps<V extends Float32Array>(baseKeys: string[][]) {
   const swizzledSet = toSwizzledSet(baseKeys);
   const keyIndexMap = toKeyIndexMap(baseKeys);
-
-  const Vec = [Vec2, Vec3, Vec4];
-
-  function getByKey(target: I, key: string): number {
-    const i = keyIndexMap.get(key) as number;
-    return target[i];
-  }
-
-  function getterFor(
-    key: string,
-  ):
-    | ((this: I) => number)
-    | ((this: I) => Vec2)
-    | ((this: I) => Vec3)
-    | ((this: I) => Vec4) {
-    const { length } = key;
-
-    return length === 1
-      ? function get(this: I) {
-          return getByKey(this, key);
-        }
-      : function get(this: I) {
-          const args = key.split('').map(k => getByKey(this, k));
-          return new Vec[length - 2](args);
-        };
-  }
-
-  function setByKey(target: I, key: string, value: number): void {
-    if (!isFinite(value)) {
-      throw new Error(
-        `Cannot assign ${typeof value} value "${value}" to a component of ${
-          target.constructor.name
-        }`,
-      );
-    }
-
-    const i = keyIndexMap.get(key) as number;
-    target[i] = value;
-  }
-
-  function setterFor(
-    key: string,
-  ):
-    | ((this: I, value: number) => void)
-    | ((this: I, value: ArrayLike<number>) => void) {
-    const { length } = key;
-
-    return length === 1
-      ? function set(this: I, value: number) {
-          setByKey(this, key, value);
-        }
-      : function set(this: I, value: ArrayLike<number>) {
-          if (length !== value.length) {
-            throw new Error(
-              [
-                'Dimension mismatch',
-                `Cannot assign ${
-                  value.length
-                }-component value: ${value} to ${length}-component key ${key}`,
-              ].join('\n'),
-            );
-          }
-
-          key.split('').forEach((k, i) => setByKey(this, k, value[i]));
-        };
-  }
 
   return Array.from(swizzledSet).reduce(
     (acc: PropertyDescriptorMap, key) => {
       acc[key] = {
         configurable: false,
         enumerable: true,
-        get: getterFor(key),
-        set: setterFor(key),
+        get: getterFor<V>(key, keyIndexMap),
+        set: setterFor<V>(key, keyIndexMap),
       };
 
       return acc;
     },
     {
       toString: {
-        value: function toString(this: I) {
+        value: function toString(this: V) {
           return `vec${this.length}(${Float32Array.prototype.toString.call(
             this,
           )})`;
