fef8cea9ad464c8d6db5a7f9e1175711c7bb57c0
Matt Hayes <matt@mysterycommand.com>
2018-12-02 01:02:08 -0500

    feat(lab/06-sprites): just messing with canvas and the resizer (which I like a lot better now)

--- src/lab/06-sprites/entry.ts
+++ src/lab/06-sprites/entry.ts
@@ -1,5 +1,27 @@
 import './style.css';
-import { el } from '../../lib/core/dom';
+
+import { el, rAF } from '../../lib/core/dom';
+import { shouldResize, resize } from './resize';
+import { ππ } from '../../lib/core/math';
 
 const c = el('canvas') as HTMLCanvasElement;
-const ctx = c.getContext('2d');
+const m = el('main') as HTMLMainElement;
+const ctx = c.getContext('2d', { alpha: false }) as CanvasRenderingContext2D;
+
+rAF(function tick(/* time */) {
+  // rAF(tick);
+
+  if (shouldResize()) {
+    resize(c, m, 20);
+  }
+
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, c.width, c.height);
+
+  ctx.beginPath();
+  ctx.arc(c.width / 2, c.height / 2, 20, 0, ππ);
+  ctx.closePath();
+
+  ctx.strokeStyle = '#fff';
+  ctx.stroke();
+});
