cdc7871d7a52f2395e3d665b1635b176bba64382
Matt Hayes <matt@mysterycommand.com>
2018-12-04 20:42:27 -0500

    feat(lib/core/math): add a randomRange function and tests

--- src/lib/core/math.test.ts
+++ src/lib/core/math.test.ts
@@ -1,6 +1,7 @@
 import {
   lerp,
   randomBool,
+  randomRange,
   saw,
   toDegrees,
   toRadians,
@@ -22,6 +23,14 @@ describe('@fartts/lib/math', () => {
     expect(randomBool()).toBe(false);
     expect(randomBool()).toBe(false);
     expect(randomBool()).toBe(true);
+  });
+
+  test('randomRange', () => {
+    expect(randomRange(0, 1)).toBe(0.3858736207545881);
+    expect(randomRange(1, 2)).toBe(1.2395959625293629);
+    expect(randomRange(1, 30)).toBe(27.889704444634948);
+    expect(randomRange(40, 20)).toBe(24.1776263921708);
+    expect(randomRange(5000, 5)).toBe(3343.64129004656);
   });
 
   test('saw', () => {
