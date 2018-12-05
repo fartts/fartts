60b2bfd5985a41031c675e69b87508d562a612b5
Matt Hayes <matt@mysterycommand.com>
2018-12-04 21:34:42 -0500

    feat(lab/07-trees): painting with trees

--- src/lab/07-trees/entry.ts
+++ src/lab/07-trees/entry.ts
@@ -1,14 +1,23 @@
 import './style.css';
 
-import { el, on, rAF, cAF } from '../../lib/core/dom';
+import { el, on, off, rAF } from '../../lib/core/dom';
 import { shouldResize, resize } from './resize';
-import { π, cos, sin, randomRange, random } from '../../lib/core/math';
+import {
+  π,
+  cos,
+  sin,
+  randomRange,
+  random,
+  toDegrees,
+} from '../../lib/core/math';
+import { sawWave } from '../../lib/core/wave';
 
 const c = el('canvas') as HTMLCanvasElement;
 const m = el('main') as HTMLMainElement;
 const ctx = c.getContext('2d') as CanvasRenderingContext2D;
 
 const totalIterations = 15;
+const saturationWave = sawWave(totalIterations, 100, 50);
 
 function branch(
   context: CanvasRenderingContext2D,
@@ -23,9 +32,16 @@ function branch(
   const as = s * randomRange(randomRange(0.6, 0.8), 0.95);
   const str = s * 0.01;
 
+  context.beginPath();
+
+  context.strokeStyle = `hsla(${toDegrees(a)}, ${saturationWave(
+    i,
+  )}%, 20%, 0.5)`;
   context.lineWidth = 8 * str;
   context.moveTo(x, y);
   context.lineTo(ax, ay);
+
+  context.stroke();
 
   if (i - 1 > 0) {
     if (random() < 0.8) {
@@ -44,15 +60,15 @@ function tree(
   y: number,
   s: number,
 ) {
-  ctx.beginPath();
   branch(context, x, y, π * 1.5, s / 5, totalIterations);
-  ctx.stroke();
 }
 
-let frameId: number;
+let pointerDown = false;
+let treeX = c.width * 0.5;
+let treeY = c.height * 0.8;
 
 function tick(/* time */) {
-  frameId = rAF(tick);
+  /* frameId =  */ rAF(tick);
 
   if (shouldResize()) {
     resize(c, m);
@@ -61,22 +77,55 @@ function tick(/* time */) {
     ctx.fillRect(0, 0, c.width, c.height);
   }
 
-  ctx.strokeStyle = `hsla(${randomRange(0, 360)}, 80%, 20%, 0.5)`;
-  tree(
-    ctx,
-    randomRange(0, c.width),
-    randomRange(c.height / 2, c.height / 2 + c.height),
-    c.width / 2,
-  );
+  if (pointerDown) {
+    tree(ctx, treeX, treeY, c.width / 4);
+  }
 }
 
-on('click', () => {
-  if (frameId === -1) {
-    frameId = rAF(tick);
-  } else {
-    cAF(frameId);
-    frameId = -1;
+function onPointerMove(event: MouseEvent | TouchEvent) {
+  if (event.type === 'mousemove') {
+    treeX = (event as MouseEvent).offsetX;
+    treeY = (event as MouseEvent).offsetY;
+    return;
   }
+
+  if (event.type === 'touchmove') {
+    treeX = (event as TouchEvent).touches[0].screenX;
+    treeY = (event as TouchEvent).touches[0].screenY;
+    return;
+  }
+}
+/* let frameId =  */ rAF(tick);
+
+// on<KeyboardEvent>('keyup', ({ code }) => {
+//   if (code !== 'Space') {
+//     return;
+//   }
+
+//   if (frameId === -1) {
+//     frameId = rAF(tick);
+//   } else {
+//     cAF(frameId);
+//     frameId = -1;
+//   }
+// });
+
+on<MouseEvent>('mousedown', () => {
+  pointerDown = true;
+  on<MouseEvent>('mousemove', onPointerMove);
 });
 
-frameId = rAF(tick);
+on<MouseEvent>('mouseup', () => {
+  pointerDown = false;
+  off<MouseEvent>('mousemove', onPointerMove);
+});
+
+on<TouchEvent>('touchdown', () => {
+  pointerDown = true;
+  on<TouchEvent>('touchmove', onPointerMove);
+});
+
+on<TouchEvent>('touchup', () => {
+  pointerDown = false;
+  off<TouchEvent>('touchmove', onPointerMove);
+});
