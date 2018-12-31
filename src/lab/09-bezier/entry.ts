d0546514035298850555a77206e4c470028fc0e7
Matt Hayes <matt@mysterycommand.com>
2018-12-31 17:23:53 -0500

    feat(lab/09): a simple resize/draw loop

--- src/lab/09-bezier/entry.ts
+++ src/lab/09-bezier/entry.ts
@@ -1,1 +1,20 @@
+import { el, rAF } from '../../lib/core/dom';
+import { resize, shouldResize } from './resize';
+
 import './style.css';
+
+const m = el('main') as HTMLMainElement;
+const c = el('canvas') as HTMLCanvasElement;
+const ctx = c.getContext('2d') as CanvasRenderingContext2D;
+
+function draw(/* ts: number */) {
+  rAF(draw);
+
+  if (shouldResize()) {
+    resize(c, m);
+  }
+
+  ctx.clearRect(0, 0, c.width, c.height);
+}
+
+rAF(draw);
