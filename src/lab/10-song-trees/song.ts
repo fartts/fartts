a62c594a639952f142dd919166b814d6b92d1a83
Matt Hayes <matt@mysterycommand.com>
2019-01-24 11:58:50 -0500

    fix(lab/10): fixes (well, works around) the mobile safari 'canplaythrough' issue

--- src/lab/10-song-trees/song.ts
+++ src/lab/10-song-trees/song.ts
@@ -3,6 +3,19 @@ import songUrl from './assets/lindenmayr-song.mp3';
 const song = new Audio(songUrl);
 
 let canPlayThrough = false;
+
+/**
+ * This preposterous hack is because Mobile Safari doesn't trigger
+ * 'canplaythrough' until you actually call song.play() which you are not
+ * allowed to do outside a UI event so we set it to autoplay and then call
+ * song.pause() as soon as it starts loading which then triggers
+ * 'canplaythrough' ... why
+ */
+song.autoplay = true;
+song.addEventListener('loadstart', () => {
+  song.pause();
+});
+
 song.addEventListener('canplaythrough', () => {
   canPlayThrough = true;
 });
