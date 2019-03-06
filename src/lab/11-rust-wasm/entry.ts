05b07fc058e03feef679d01516bd4b865d2bded4
Matt Hayes <matt@mysterycommand.com>
2019-03-06 16:39:36 -0500

    feat(lab/11): resize the canvas and rendering context

--- src/lab/11-rust-wasm/entry.ts
+++ src/lab/11-rust-wasm/entry.ts
@@ -1,1 +1,15 @@
+import { el } from '../../lib/core/dom';
+import { max } from '../../lib/core/math';
+
 import './style.css';
+
+const { devicePixelRatio: dpr } = window;
+
+const c = el('canvas') as HTMLCanvasElement;
+
+c.width = 720 * dpr;
+c.height = 480 * dpr;
+c.style.transform = `scale(${max(720 / c.width, 480 / c.height)})`;
+
+const ctx = c.getContext('2d') as CanvasRenderingContext2D;
+console.log(ctx); // tslint:disable-line
