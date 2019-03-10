e16c30a5e03ceb3d14d43e89184c9fcdcec888f8
Matt Hayes <matt@mysterycommand.com>
2019-03-10 12:52:00 -0500

    feat(lab/12): create a screen state closure, keep a store up to date and expose getters

--- src/lab/12-wasm-particles/entry.ts
+++ src/lab/12-wasm-particles/entry.ts
@@ -1,49 +1,9 @@
-import { el } from '../../lib/core/dom';
-import { max } from '../../lib/core/math';
+import { screen } from './screen';
 
 import './style.css';
 
-function on(
-  target: EventTarget,
-  type: string,
-  listener: EventListener,
-  useCapture: boolean = false,
-) {
-  target.addEventListener(type, listener, useCapture);
-}
+const s = screen();
 
-function step(by = 1, from = 0) {
-  if (from % by === 0) {
-    // "from" is already a multiple/step of "by", we're done
-    return from;
-  }
-
-  return from + (by - (from % by));
-}
-
-const { devicePixelRatio: dpr } = window;
-
-const main = el('main') as HTMLMainElement;
-const canvas = el('canvas') as HTMLCanvasElement;
-const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-let { clientWidth: w, clientHeight: h } = main;
-on(window, 'resize', () => {
-  w = main.clientWidth;
-  h = main.clientHeight;
-
-  const scale = 10;
-  const sw = step(scale, w);
-  const sh = step(scale, h);
-
-  canvas.width = (sw * dpr) / scale;
-  canvas.height = (sh * dpr) / scale;
-
-  const cover = max(w / canvas.width, h / canvas.height);
-  // const contain = min(w / canvas.width, h / canvas.height);
-  canvas.style.transform = `scale(${cover})`;
+window.addEventListener('resize', () => {
+  console.log(s); // tslint:disable-line
 });
-window.dispatchEvent(new Event('resize'));
-
-console.log(context); // tslint:disable-line
-// draw(context, canvas.width, canvas.height);
