d22f561b4e1039e3eaa193ffbbaba73702a0a046
Matt Hayes <matt@mysterycommand.com>
2019-03-28 22:29:31 -0400

    feat(lab/12): add global mutable state to Rust
    hi, you're welcome

--- src/lab/12-wasm-particles/lab-12/src/lib.rs
+++ src/lab/12-wasm-particles/lab-12/src/lib.rs
@@ -1,10 +1,15 @@
+#[macro_use]
+extern crate lazy_static;
+
 extern crate cfg_if;
+extern crate mut_static;
 extern crate wasm_bindgen;
 extern crate web_sys;
 
 mod utils;
 
 use cfg_if::cfg_if;
+use mut_static::MutStatic;
 use std::f64::consts::PI;
 use wasm_bindgen::prelude::*;
 use web_sys::CanvasRenderingContext2d;
@@ -20,26 +25,41 @@ cfg_if! {
 }
 
 // let mut rs: 'static f64 = 0.0;
+pub struct State {
+    pub t: f32,
+}
 
-#[wasm_bindgen]
-pub fn update() {
-    /* t: f32, dt: f32 */
+impl State {
+    pub fn new() -> State {
+        State { t: 0.0 }
+    }
+}
+
+lazy_static! {
+    pub static ref STATE: MutStatic<State> = { MutStatic::from(State::new()) };
 }
 
 #[wasm_bindgen]
-pub fn draw(ctx: &CanvasRenderingContext2d, w: u32, h: u32) {
+pub extern "C" fn update(t: f32) {
+    let mut state = STATE.write().unwrap();
+    state.t = t;
+}
+
+#[wasm_bindgen]
+pub extern "C" fn draw(ctx: &CanvasRenderingContext2d, w: u32, h: u32) {
+    let state = STATE.read().unwrap();
+
+    let s = f64::from(state.t.sin()) + 1.0;
+    let tw = f64::from(w) / 3.0;
+    let th = f64::from(h) / 3.0;
+    let r = tw.min(th) * s;
+
     ctx.set_fill_style(&JsValue::from_str("red"));
     ctx.fill_rect(0.0, 0.0, w.into(), h.into());
 
     ctx.set_fill_style(&JsValue::from_str("white"));
-    ctx.arc(
-        (w / 2).into(),
-        (h / 2).into(),
-        (w / 4).min(h / 4).into(),
-        0.0,
-        PI * 2.0,
-    )
-    .unwrap();
+    ctx.arc(f64::from(w) / 2.0, f64::from(h) / 2.0, r, 0.0, PI * 2.0)
+        .unwrap();
     ctx.fill();
 }
 
