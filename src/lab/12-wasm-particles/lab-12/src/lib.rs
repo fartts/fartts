d95e6a1cfdc1aa107cf5adb3be78c2d9a0019ef3
Matt Hayes <matt@mysterycommand.com>
2019-03-29 12:50:56 -0400

    feat(lab/12): add 10,000 black particles, it's rough, but it sorta works

--- src/lab/12-wasm-particles/lab-12/src/lib.rs
+++ src/lab/12-wasm-particles/lab-12/src/lib.rs
@@ -3,6 +3,7 @@ extern crate lazy_static;
 
 extern crate cfg_if;
 extern crate mut_static;
+extern crate rand;
 extern crate wasm_bindgen;
 extern crate web_sys;
 
@@ -32,28 +33,41 @@ lazy_static! {
 }
 
 #[wasm_bindgen]
-pub fn update(t: f32) {
+pub fn update(t: f64) {
     let mut state = STATE.write().unwrap();
     state.t = t;
+
+    for p in state.p.iter_mut() {
+        p.update();
+    }
 }
 
 #[wasm_bindgen]
 pub fn draw(ctx: &CanvasRenderingContext2d, w: u32, h: u32) {
     let state = STATE.read().unwrap();
 
+    let hw = f64::from(w) / 2.0;
+    let hh = f64::from(h) / 2.0;
     let s = (f64::from((state.t / 500.0).sin()) + 1.0) / 2.0;
-    let tw = f64::from(w) / 6.0;
-    let th = f64::from(h) / 6.0;
-    let r = tw.min(th) + tw.min(th) * s;
+    let sw = f64::from(w) / 6.0;
+    let sh = f64::from(h) / 6.0;
+    let r = sw.min(sh) + sw.min(sh) * s;
 
     ctx.set_fill_style(&JsValue::from_str("red"));
     ctx.fill_rect(0.0, 0.0, w.into(), h.into());
 
     ctx.set_fill_style(&JsValue::from_str("white"));
     ctx.begin_path();
-    ctx.arc(f64::from(w) / 2.0, f64::from(h) / 2.0, r, 0.0, PI * 2.0)
-        .unwrap();
+    ctx.arc(hw, hh, r, 0.0, PI * 2.0).unwrap();
     ctx.fill();
+
+    for p in state.p.iter() {
+        ctx.set_fill_style(&JsValue::from_str("black"));
+        ctx.begin_path();
+        ctx.arc(hw + p.curr_pos.x, hh + p.curr_pos.y, 5.0, 0.0, PI * 2.0)
+            .unwrap();
+        ctx.fill();
+    }
 }
 
 #[cfg(test)]
--- src/lab/12-wasm-particles/lab-12/src/state.rs
+++ src/lab/12-wasm-particles/lab-12/src/state.rs
@@ -1,9 +1,71 @@
+use rand::prelude::*;
+use std::f64::consts::PI;
+
+pub struct Vec2 {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Vec2 {
+    pub fn new(x: f64, y: f64) -> Vec2 {
+        Vec2 { x: x, y: y }
+    }
+}
+
+pub struct Particle {
+    pub curr_pos: Vec2,
+    pub prev_pos: Vec2,
+}
+
+impl Particle {
+    pub fn new(cx: f64, cy: f64, px: f64, py: f64) -> Particle {
+        Particle {
+            curr_pos: Vec2::new(cx, cy),
+            prev_pos: Vec2::new(px, py),
+        }
+    }
+
+    pub fn update(&mut self) {
+        let prev_vel = Vec2::new(
+            self.curr_pos.x - self.prev_pos.x,
+            self.curr_pos.y - self.prev_pos.y,
+        );
+
+        let next_vel = Vec2::new(prev_vel.x, prev_vel.y + 0.1); // gravity
+        let next_vel = Vec2::new(next_vel.x * 0.99, next_vel.y * 0.99); // drag
+
+        self.prev_pos = Vec2::new(self.curr_pos.x, self.curr_pos.y);
+        self.curr_pos = Vec2::new(self.curr_pos.x + next_vel.x, self.curr_pos.y + next_vel.y);
+
+        if self.curr_pos.y > 1000.0 {
+            let a = PI * 2.0 * random::<f64>();
+            let r = 10.0 * random::<f64>();
+            let px = a.cos() * r;
+            let py = a.sin() * r;
+
+            self.prev_pos = Vec2::new(px, py);
+            self.curr_pos = Vec2::new(0.0, 0.0);
+        }
+    }
+}
+
 pub struct State {
-    pub t: f32,
+    pub t: f64,
+    pub p: Vec<Particle>,
 }
 
 impl State {
     pub fn new() -> State {
-        State { t: 0.0 }
+        let mut p = Vec::new();
+
+        for _ in 0..10000 {
+            let a = PI * 2.0 * random::<f64>();
+            let r = 10.0 * random::<f64>();
+            let px = a.cos() * r;
+            let py = a.sin() * r;
+            p.push(Particle::new(0.0, 0.0, px, py))
+        }
+
+        State { t: 0.0, p: p }
     }
 }
