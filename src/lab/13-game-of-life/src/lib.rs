9984d0bee526201f63b65bcbc9498630fe7af720
Matt Hayes <matt@mysterycommand.com>
2019-05-13 13:19:44 -0400

    feat(lab/13): boilerplate for a new experiment
    gonna implement Conway's Game of Life following -
    https://rustwasm.github.io/docs/book/game-of-life/rules.html

---
+++ src/lab/13-game-of-life/src/lib.rs
@@ -0,0 +1,36 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+extern crate web_sys;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+pub fn run() {
+    // If the `console_error_panic_hook` feature is enabled this will set a
+    // panic hook, otherwise it will do nothing.
+    utils::set_panic_hook();
+
+    console::log_1(&JsValue::from_str("run"));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
