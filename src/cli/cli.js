39148cd9183e9429272d911c7a738a29ac5064c2
Matt Hayes <matt@mysterycommand.com>
2019-05-22 15:53:32 -0400

    refactor(cli): move writing templates to a relative path into it's own function

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -15,32 +15,43 @@ const files = {
   'src/utils.rs': require('./templates/src/utils.rs.js'),
 };
 
+const exists = util.promisify(fs.exists);
+const mkdir = util.promisify(fs.mkdir);
+const writeFile = util.promisify(fs.writeFile);
+
+async function writeFromTemplate(relPath, tpl) {
+  const filePath = path.join(experimentDir, relPath);
+  const fileDir = path.dirname(filePath);
+  const fileContents = tpl({ name: experimentName });
+
+  if (!(await exists(fileDir))) {
+    await mkdir(fileDir);
+  }
+
+  if (await exists(filePath)) {
+    console.warn(
+      `file already exists at ${path.relative(
+        process.cwd(),
+        filePath,
+      )} skipping`,
+    );
+
+    return;
+  }
+
+  return writeFile(filePath, fileContents);
+}
+
 module.exports = function cli(args) {
   args.forEach(arg => {
     const { length } = fs.readdirSync(labDir);
     const experimentName = kebabCase(`${length - 3}${arg}`);
     const experimentDir = path.join(labDir, experimentName);
 
-    Object.entries(files).forEach(([relPath, tpl]) => {
-      const filePath = path.join(experimentDir, relPath);
-      const fileDir = path.dirname(filePath);
-      const fileContents = tpl({ name: experimentName });
-
-      fs.existsSync(fileDir) || fs.mkdirSync(fileDir);
-
-      if (fs.existsSync(filePath)) {
-        console.warn(
-          `file already exists at ${path.relative(
-            process.cwd(),
-            filePath,
-          )} skipping`,
-        );
-
-        return;
-      }
-
-      fs.writeFileSync(filePath, fileContents);
-    });
+    const entries = Object.entries(files);
+    for (const [relPath, tpl] of entries) {
+      await writeFromTemplate(relPath, tpl);
+    }
 
     const cargoPath = path.join(process.cwd(), 'Cargo.toml');
     const {
