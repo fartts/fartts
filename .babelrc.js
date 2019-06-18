d91351703b43f2d2e98d99af2562a6466a30d272
Matt Hayes <matt@mysterycommand.com>
2019-06-18 11:17:11 -0400

    build(.babelrc.js): add @babel/plugin-syntax-import-meta
    fixes the build, but it'll still complain at runtime

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,8 @@
+/**
+ * NOTE: this *must* be in a `.babelrc` or `.babelrc.js` file
+ * @see: https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/transforms/babel/babelrc.js#L73
+ */
 module.exports = {
   presets: ['@babel/typescript'],
+  plugins: ['@babel/syntax-import-meta'],
 };
