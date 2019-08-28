1d221a23c1f31a5e1ae7482347235c7dcf0dcc7e
Matt Hayes <matt@mysterycommand.com>
2019-08-28 00:11:11 -0400

    refactor(cli): refinaggle some templates, start updating the cli

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,101 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
+const createLab = require('./utils/create-lab');
 
-const cheerio = require('cheerio');
-const { kebabCase, words } = require('lodash');
-const prettier = require('prettier');
-const { parse, stringify } = require('@iarna/toml');
-
-const labsDir = path.join(__dirname, '../lab');
-const files = {
-  'Cargo.toml': require('./templates/wasm/Cargo.toml.js/index.js'),
-  'index.html': require('./templates/wasm/index.html.js/index.js'),
-  'src/entry.ts': require('./templates/wasm/src/entry.ts.js'),
-  'src/lib.rs': require('./templates/wasm/src/lib.rs.js'),
-  'src/style.css': require('./templates/wasm/src/style.css.js'),
-  'src/utils.rs': require('./templates/wasm/src/utils.rs.js'),
+const commonFiles = {
+  'index.html': require('./templates/common/index-html.js'),
+  'src/style.css': require('./templates/common/src/style-css'),
 };
 
-const mkdir = util.promisify(fs.mkdir);
-const readdir = util.promisify(fs.readdir);
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
+const wasmFiles = {
+  'Cargo.toml': require('./templates/wasm/Cargo-toml'),
+  'src/entry.ts': require('./templates/wasm/src/entry-ts'),
+  'src/lib.rs': require('./templates/wasm/src/lib-rs'),
+  'src/utils.rs': require('./templates/wasm/src/utils-rs'),
+};
 
-async function createLab(arg) {
-  const { length } = await readdir(labsDir);
+const files = {
+  wasm: {
+    ...commonFiles,
+    ...wasmFiles,
+  },
+};
 
-  const labName = kebabCase(arg);
-  const labNumber = kebabCase(`${length - 3}-${labName}`);
-  const labDir = path.join(labsDir, labNumber);
-
-  const entries = Object.entries(files);
-  for (const [relPath, tpl] of entries) {
-    const filePath = path.join(labDir, relPath);
-    const fileContents = tpl({ labName, labNumber });
-
-    await createFile(filePath, fileContents);
-  }
-
-  await updateCargo(labDir);
-  await updateIndex(labName, labNumber);
-
-  return;
-}
-
-async function createFile(filePath, fileContents) {
-  const fileDir = path.dirname(filePath);
-
-  try {
-    await mkdir(fileDir);
-  } catch {}
-
-  return writeFile(filePath, fileContents, { flag: 'wx' }).catch(() => {
-    console.warn(
-      `file already exists at ${path.relative(
-        process.cwd(),
-        filePath,
-      )} skipping`,
-    );
-  });
-}
-
-async function updateCargo(labDir) {
-  const cargoPath = path.join(process.cwd(), 'Cargo.toml');
-  const cargo = parse(await readFile(cargoPath));
-
-  const {
-    workspace: { members },
-  } = cargo;
-  const cargoContents = stringify({
-    ...cargo,
-    workspace: {
-      members: [...new Set([...members, path.relative(process.cwd(), labDir)])],
-    },
-  });
-
-  return writeFile(cargoPath, cargoContents, { flag: 'w' }).catch(
-    () => undefined,
-  );
-}
-
-async function updateIndex(labName, labNumber) {
-  const indexPath = path.join(labsDir, 'index.html');
-  const index = cheerio.load(await readFile(indexPath));
-
-  const linkText = words(labName).join(' ');
-  index('ul').append(
-    `<li><a href="./${labNumber}/index.html">${linkText}</a></li>`,
-  );
-  const indexContents = prettier.format(index.html(), { parser: 'html' });
-
-  return writeFile(indexPath, indexContents, { flag: 'w' }).catch(
-    () => undefined,
-  );
-}
-
-module.exports = async function cli(args) {
+module.exports = async function cli([cmd, ...args]) {
   for (const arg of args) {
-    await createLab(arg);
+    await createLab(files[cmd], arg, { updateCargo: cmd === 'wasm' });
   }
 };
