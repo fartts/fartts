11f66eca89b5ad9083f882f21b4aedc05439a7ca
Matt Hayes <matt@mysterycommand.com>
2019-08-28 11:53:47 -0400

    refactor(lab/14): move the color into a little hsl function

--- src/lab/14-maximally-minimal-ecs/src/entry.ts
+++ src/lab/14-maximally-minimal-ecs/src/entry.ts
@@ -9,15 +9,18 @@ const { round, random } = Math;
 const m = el('main') as HTMLElement;
 const c = el('canvas') as HTMLCanvasElement;
 const ctx = c.getContext('2d') as CanvasRenderingContext2D;
-const s = 10;
+const scale = 10;
+
+const hsl = (h = round(random() * 360), s = 60, l = 40) =>
+  `hsl(${h}, ${s}%, ${l}%)`;
 
 rAF(function step() {
   rAF(step);
 
   if (resizer.shouldResize) {
-    resizer.resize(m, c, s);
+    resizer.resize(m, c, scale);
 
-    ctx.fillStyle = `hsl(${round(random() * 360)}, 50%, 50%)`;
+    ctx.fillStyle = hsl();
     ctx.fillRect(0, 0, c.width, c.height);
   }
 });
