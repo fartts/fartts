f26e5dfd1c41c34c67b14b733ee710e008544a89
Matt Hayes <matt@mysterycommand.com>
2019-08-27 23:13:20 -0400

    chore(**/*/cargo.toml): bump cargo deps

--- src/cli/templates/Cargo.toml.js
+++ src/cli/templates/Cargo.toml.js
@@ -13,16 +13,16 @@ default = ["console_error_panic_hook", "wee_alloc"]
 
 [dependencies]
 console_error_panic_hook = { version = "0.1.6", optional = true }
-js-sys = "0.3.22"
-wasm-bindgen = "0.2.45"
-wee_alloc = { version = "0.4.4", optional = true }
+js-sys = "0.3.27"
+wasm-bindgen = "0.2.50"
+wee_alloc = { version = "0.4.5", optional = true }
 
 [dependencies.web-sys]
-version = "0.3.22"
+version = "0.3.27"
 features = [
   "console",
 ]
 
 [dev-dependencies]
-wasm-bindgen-test = "0.2.45"
+wasm-bindgen-test = "0.2.50"
 `;
