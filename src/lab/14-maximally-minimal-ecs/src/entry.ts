40b85867e27491904ec456e8537f84ae0659930d
Matt Hayes <matt@mysterycommand.com>
2019-08-28 11:42:09 -0400

    feat(lab/14): simple setup, some new TypeScript learning in here, feels good

--- src/lab/14-maximally-minimal-ecs/src/entry.ts
+++ src/lab/14-maximally-minimal-ecs/src/entry.ts
@@ -1,3 +1,22 @@
+import { rAF, el } from '../../../lib/core/dom';
+import resizer from './resizer';
+
 import './style.css';
 
-console.log('run');
+const { round, random } = Math;
+
+const m = el('main') as HTMLElement;
+const c = el('canvas') as HTMLCanvasElement;
+const ctx = c.getContext('2d') as CanvasRenderingContext2D;
+const s = 10;
+
+rAF(function step() {
+  rAF(step);
+
+  if (resizer.shouldResize) {
+    resizer.resize(m, c, s);
+
+    ctx.fillStyle = `hsl(${round(random() * 360)}, 50%, 50%)`;
+    ctx.fillRect(0, 0, c.width, c.height);
+  }
+});
