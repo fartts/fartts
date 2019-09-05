6d3fef81895b81813baa602e1ab3c0adeb566be6
Matt Hayes <matt@mysterycommand.com>
2019-09-04 17:21:43 -0700

    feat(lab/14): handle mouse enter, leave, up and down, also canvas resize

--- src/lab/14-an-ecs/src/input.ts
+++ src/lab/14-an-ecs/src/input.ts
@@ -1,19 +1,48 @@
-import { attend } from './events';
+import { attend, ignore } from './events';
 import { c } from './page';
 
 let mouseX = c.width / 2;
 let mouseY = c.height / 2;
+let mouseDown = false;
 
-attend(c, 'mousemove', ({ clientX, clientY }) => {
+function onMouseMove({ clientX, clientY }: MouseEvent) {
   mouseX = clientX;
   mouseY = clientY;
+}
+
+new MutationObserver(() => {
+  mouseX = c.width / 2;
+  mouseY = c.height / 2;
+}).observe(c, { attributes: true, attributeFilter: ['width', 'height'] });
+
+attend(c, 'mouseenter', ({ buttons }) => {
+  attend(c, 'mousemove', onMouseMove);
+  mouseDown = buttons !== 0;
+});
+
+attend(c, 'mouseleave', ({ buttons }) => {
+  ignore(c, 'mousemove', onMouseMove);
+  mouseX = c.width / 2;
+  mouseY = c.height / 2;
+  mouseDown = buttons !== 0;
+});
+
+attend(c, 'mousedown', ({ buttons }) => {
+  mouseDown = buttons !== 0;
+});
+
+attend(c, 'mouseup', ({ buttons }) => {
+  mouseDown = buttons !== 0;
 });
 
 export const input = {
   get mouseX() {
-    return mouseY;
+    return mouseX;
   },
   get mouseY() {
-    return mouseX;
+    return mouseY;
+  },
+  get mouseDown() {
+    return mouseDown;
   },
 };
