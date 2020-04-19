e9dea06ccb22c458f7984a4f847257c8734202e5
Matt Hayes <matt@mysterycommand.com>
2020-04-18 23:17:52 -0400

    feat(lab/17): a fixed timestep is not trivial

--- src/lab/17-collisions/src/entry.ts
+++ src/lab/17-collisions/src/entry.ts
@@ -16,17 +16,45 @@ if (!context) {
 const pixelScale = 12;
 let shouldResize = true;
 
+const frameTime = 1_000 / 60;
+let firstTime = 0;
+let previousTime = 0;
+let overTime = 0;
+
 on(window, 'resize', () => {
   shouldResize = true;
 });
 
-const tick = () => {
+const tick = (time: DOMHighResTimeStamp) => {
   rAF(tick);
+
+  firstTime || (firstTime = time); // tslint:disable-line:no-unused-expression
 
   if (shouldResize) {
     resize(main, canvas, pixelScale);
     shouldResize = false;
+
+    firstTime = time;
+    previousTime = 0;
+    overTime = 0;
   }
+
+  const normalTime = time - firstTime;
+  const deltaTime = normalTime - previousTime;
+  overTime += deltaTime;
+
+  if (normalTime === 0) {
+    // create();
+  }
+
+  while (overTime >= frameTime) {
+    previousTime += frameTime;
+    // update(previousTime, frameTime);
+    overTime -= frameTime;
+  }
+
+  // render(context);
+  previousTime = normalTime;
 };
 
 rAF(tick);
