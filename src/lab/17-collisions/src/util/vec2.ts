5e27da1e49cd288e52ec2da7ee52bc35f5998189
Matt Hayes <matt@mysterycommand.com>
2020-06-28 17:06:23 -0400

    feat(lab/17/vec2): add dot and cross product and a normalize function
    fuss with point of intesection function, add some docs

--- src/lab/17-collisions/src/util/vec2.ts
+++ src/lab/17-collisions/src/util/vec2.ts
@@ -8,28 +8,52 @@ export interface Vec2 {
 export const vec2: (x: number, y: number) => Vec2 = (x, y) => ({ x, y });
 
 export const copy: (v: Vec2) => Vec2 = (v) => vec2(v.x, v.y);
+
 export const addv: (a: Vec2, b: Vec2) => Vec2 = (a, b) =>
   vec2(a.x + b.x, a.y + b.y);
+
 export const subv: (a: Vec2, b: Vec2) => Vec2 = (a, b) =>
   vec2(a.x - b.x, a.y - b.y);
+
 export const muls: (a: Vec2, s: number) => Vec2 = (a, s) =>
   vec2(a.x * s, a.y * s);
+
 export const size: (v: Vec2) => number = ({ x, y }) => hypot(x, y);
 
+export const dotp: (a: Vec2, b: Vec2) => number = (a, b) =>
+  a.x * b.x + a.y * b.y;
+
+export const crss: (a: Vec2, b: Vec2) => number = (a, b) =>
+  a.x * b.y - a.y * b.x;
+
+export const nrml: (v: Vec2) => Vec2 = (v) => muls(v, 1 / size(v));
+
+/**
+ * point of intesection (between two line segments)
+ *
+ * @returns Vec2
+ * @see https://stackoverflow.com/a/24392281/1577876
+ * @see https://stackoverflow.com/a/30160064/1577876
+ */
 export const poiv: (a: [Vec2, Vec2], b: [Vec2, Vec2]) => Vec2 = (
   [a, b],
   [c, d],
 ) => {
-  const det = (b.x - a.x) * (d.y - c.y) - (d.x - c.x) * (b.y - a.y);
+  const ab = subv(b, a);
+  const cd = subv(d, c);
+
+  const det = crss(ab, cd);
 
   if (det === 0) {
+    // the two segments are parallel and do not intersect
     return vec2(NaN, NaN);
   }
 
-  const lambda = ((d.y - c.y) * (d.x - a.x) + (c.x - d.x) * (d.y - a.y)) / det;
-  const gamma = ((a.y - b.y) * (d.x - a.x) + (b.x - a.x) * (d.y - a.y)) / det;
+  const t = subv(d, a);
+  const lambda = ((d.y - c.y) * t.x + (c.x - d.x) * t.y) / det;
+  const gamma = ((a.y - b.y) * t.x + (b.x - a.x) * t.y) / det;
 
-  return 0 < lambda && lambda < 1 && 0 < gamma && gamma < 1
-    ? addv(a, muls(subv(b, a), lambda))
-    : vec2(NaN, NaN);
+  return -0.01 < lambda && lambda < 1.01 && -0.01 < gamma && gamma < 1.01
+    ? addv(a, muls(ab, lambda)) // could also use: `addv(c, muls(cd, gamma))`
+    : vec2(NaN, NaN); // the two segments do not intersect
 };
