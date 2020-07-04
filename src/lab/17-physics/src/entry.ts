22291dee2dba5b50f20aca63f33d570ffe5f67d1
Matt Hayes <matt@mysterycommand.com>
2020-07-04 12:00:57 -0400

    feat(lab/17): boilerplate, and an initial screenshot

--- src/lab/17-physics/src/entry.ts
+++ src/lab/17-physics/src/entry.ts
@@ -1,3 +1,31 @@
+import { el, min, on, ππ } from './lib/browser-utils';
+import { resize } from './lib/resize';
+
 import './style.css';
 
-console.log('run');
+const main = el<HTMLElement>('main');
+const canvas = el<HTMLCanvasElement>('canvas');
+const context = canvas.getContext('2d', {
+  alpha: false,
+  desynchronized: true,
+});
+
+if (!context) {
+  throw new Error("Couldn't get a `CanvasRenderingContext2D`");
+}
+
+let shouldResize = true;
+on(window, 'resize', () => {
+  shouldResize = true;
+});
+
+if (shouldResize) {
+  resize(main, canvas, 30);
+}
+
+const { width: w, height: h } = canvas;
+const r = min(w, h) / 4;
+
+context.fillStyle = '#4cbb17';
+context.ellipse(w / 2, h / 2, r, r, 0, 0, ππ);
+context.fill();
