2cde5dea4b3d71f798577b6f066573eaa2b4aed5
Matt Hayes <matt@mysterycommand.com>
2020-12-28 20:04:26 -0600

    feat(lab 19): a round rect implementation, it's something

--- src/lab/19-ponds/src/entry.ts
+++ src/lab/19-ponds/src/entry.ts
@@ -1,9 +1,11 @@
-import { floor, min, ππ } from '../../../lib/core/math';
+import { floor, min, π, ππ } from '../../../lib/core/math';
 import './style.css';
 
 interface Listener<T extends keyof DocumentEventMap> extends EventListener {
   (event: DocumentEventMap[T]): void;
 }
+
+const hπ = π / 2;
 
 const el = <T extends keyof HTMLElementTagNameMap>(
   selector: T,
@@ -38,28 +40,60 @@ const resize = () => {
   draw();
 };
 
-const draw = () => {
+const bkgd = () => {
   const { width: w, height: h } = canvas;
+
+  ctx.fillStyle = 'hsl(100, 40%, 60%)';
+  ctx.fillRect(0, 0, w, h);
+};
+
+const roundRect = (x: number, y: number, w: number, h: number, r: number) => {
+  ctx.beginPath();
+
+  // top left
+  ctx.arc(x + r, y + r, r, π, π + hπ);
+  ctx.lineTo(x + w - r, y);
+
+  // top right
+  ctx.arc(x + w - r, y + r, r, π + hπ, 0);
+  ctx.lineTo(x + w, y + h - r);
+
+  // bottom right
+  ctx.arc(x + w - r, y + h - r, r, 0, hπ);
+  ctx.lineTo(x + w - r, y + h);
+
+  // bottom left
+  ctx.arc(x + r, y + h - r, r, hπ, π);
+  ctx.lineTo(x, y + r);
+
+  ctx.closePath();
+};
+
+const pond = () => {
+  const { width: w, height: h } = canvas;
+
+  const rw = w * 0.8;
+  const rh = h * 0.8;
 
   const hw = w / 2;
   const hh = h / 2;
 
-  const r = min(hw, hh) * 0.8;
-  const line = min(w, h) * 0.025;
-  const circ = ππ * r;
+  const r = min(hw, hh) * 0.4;
+  const line = min(rw, rh) * 0.025;
+  const circ = ππ * r + (rw - r * 2) * 2 + (rh - r * 2) * 2;
   const dash = circ / 30;
-
-  ctx.fillStyle = 'hsl(100, 40%, 60%)';
-  ctx.fillRect(0, 0, w, h);
 
   ctx.strokeStyle = 'hsl(50, 10%, 90%)';
   ctx.lineWidth = line;
   ctx.setLineDash([dash * (17 / 32), dash * (15 / 32)]);
 
-  ctx.beginPath();
-  ctx.arc(hw, hh, r, 0, ππ);
-  ctx.closePath();
+  roundRect(hw - rw / 2, hh - rh / 2, rw, rh, r);
   ctx.stroke();
+};
+
+const draw = () => {
+  bkgd();
+  pond();
 };
 
 on(window, 'resize', resize);
