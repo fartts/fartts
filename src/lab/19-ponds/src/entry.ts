fa48bd0f4ae321779147cad94b4d04d4570c3462
Matt Hayes <matt@mysterycommand.com>
2020-12-29 13:14:57 -0600

    refactor(lab 19): split things up, make everthing take in a state object as it's first arg

--- src/lab/19-ponds/src/entry.ts
+++ src/lab/19-ponds/src/entry.ts
@@ -5,6 +5,23 @@ interface Listener<T extends keyof DocumentEventMap> extends EventListener {
   (event: DocumentEventMap[T]): void;
 }
 
+interface State {
+  ctx: CanvasRenderingContext2D;
+  canvasWidth: number;
+  canvasHeight: number;
+  safeWidth: number;
+  safeHeight: number;
+  halfWidth: number;
+  halfHeight: number;
+  r: number;
+  step: number;
+  rectWidth: number;
+  rectHeight: number;
+  lineWidth: number;
+  C: number;
+  dash: number;
+}
+
 const hπ = π / 2;
 
 const el = <T extends keyof HTMLElementTagNameMap>(
@@ -40,11 +57,9 @@ const resize = () => {
   draw();
 };
 
-const bkgd = () => {
-  const { width: w, height: h } = canvas;
-
+const bkgd = ({ ctx, canvasWidth, canvasHeight }: State) => {
   ctx.fillStyle = 'hsl(100, 40%, 60%)';
-  ctx.fillRect(0, 0, w, h);
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 };
 
 /**
@@ -52,7 +67,14 @@ const bkgd = () => {
  * @see https://www.chromestatus.com/feature/5678204184428544
  * @see https://github.com/fserb/canvas2D/blob/master/spec/roundrect.md
  */
-const roundRect = (x: number, y: number, w: number, h: number, r: number) => {
+const roundRect = (
+  { ctx }: State,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  r: number,
+) => {
   ctx.beginPath();
 
   // top left
@@ -74,30 +96,35 @@ const roundRect = (x: number, y: number, w: number, h: number, r: number) => {
   ctx.closePath();
 };
 
-const gridRect = (x: number, y: number, w: number, h: number, s: number) => {
-  const { width: cw, height: ch } = canvas;
-
+const gridRect = (
+  { ctx, canvasWidth, canvasHeight }: State,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  s: number,
+) => {
   ctx.beginPath();
 
   for (let i = 0; i <= w; i += s) {
     ctx.moveTo(x + i, 0);
-    ctx.lineTo(x + i, ch);
+    ctx.lineTo(x + i, canvasHeight);
   }
 
   for (let j = 0; j <= h; j += s) {
     ctx.moveTo(0, y + j);
-    ctx.lineTo(cw, y + j);
+    ctx.lineTo(canvasWidth, y + j);
   }
 };
 
-const pond = () => {
-  const { width: w, height: h } = canvas;
+const getState = (): State => {
+  const { width: canvasWidth, height: canvasHeight } = canvas;
 
-  const safeWidth = w * 0.8;
-  const safeHeight = h * 0.8;
+  const safeWidth = canvasWidth * 0.8;
+  const safeHeight = canvasHeight * 0.8;
 
-  const halfWidth = w / 2;
-  const halfHeight = h / 2;
+  const halfWidth = canvasWidth / 2;
+  const halfHeight = canvasHeight / 2;
 
   const r = min(halfWidth, halfHeight) * 0.2;
   const step = r;
@@ -109,12 +136,43 @@ const pond = () => {
   const C = ππ * r + (rectWidth - r * 2) * 2 + (rectHeight - r * 2) * 2;
   const dash = C / 30;
 
+  return {
+    ctx,
+    canvasWidth,
+    canvasHeight,
+    safeWidth,
+    safeHeight,
+    halfWidth,
+    halfHeight,
+    r,
+    step,
+    rectWidth,
+    rectHeight,
+    lineWidth,
+    C,
+    dash,
+  };
+};
+
+const pond = (state: State) => {
+  const {
+    ctx,
+    halfWidth,
+    halfHeight,
+    rectWidth,
+    rectHeight,
+    lineWidth,
+    dash,
+    r,
+  } = state;
+
   // draw a pond
   ctx.strokeStyle = 'hsl(50, 10%, 90%)';
   ctx.lineWidth = lineWidth;
 
   ctx.setLineDash([dash * (17 / 32), dash * (15 / 32)]);
   roundRect(
+    state,
     halfWidth - rectWidth / 2,
     halfHeight - rectHeight / 2,
     rectWidth,
@@ -122,6 +180,10 @@ const pond = () => {
     r,
   );
   ctx.stroke();
+};
+
+const grid = (state: State) => {
+  const { ctx, halfWidth, halfHeight, rectWidth, rectHeight, step } = state;
 
   // draw a grid
   ctx.strokeStyle = 'hsl(0, 0%, 20%)';
@@ -129,6 +191,7 @@ const pond = () => {
 
   ctx.setLineDash([]);
   gridRect(
+    state,
     halfWidth - rectWidth / 2,
     halfHeight - rectHeight / 2,
     rectWidth,
@@ -139,8 +202,10 @@ const pond = () => {
 };
 
 const draw = () => {
-  bkgd();
-  pond();
+  const state = getState();
+  bkgd(state);
+  pond(state);
+  grid(state);
 };
 
 on(window, 'resize', resize);
